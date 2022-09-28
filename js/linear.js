74c7e396c0c715b2749d8a21b999756773194d1d
Naloyka <elena.naloeva@yandex.ru>
2022-09-28 17:12:30 +0300

    mobile version-2

--- js/linear.js
+++ js/linear.js
@@ -34,14 +34,10 @@ const optionArc = {
     endSocketGravity: 400,
 }
 
-let wbLineBig = new LeaderLine(LeaderLine.pointAnchor(wb, { x: 15, y: 85 }), LeaderLine.pointAnchor(ecomImg, { x: 100, y: 20 }), optionLine)
-let aliexpressLineBig = new LeaderLine(LeaderLine.pointAnchor(aliexpress, { x: 15, y: 15 }), LeaderLine.pointAnchor(ecomImg, { x: 100, y: 100 }), optionLine)
-let marketAliexpressLineBig = new LeaderLine(LeaderLine.pointAnchor(marketplaseAliexpress, { x: 75, y: 48 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 30, y: 160 }), optionArc)
-let marketYandexLineBig = new LeaderLine(LeaderLine.pointAnchor(marketplaseYandex, { x: 110, y: 48 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 110, y: 300 }), optionArc)
-let marketSberbankLineBig = new LeaderLine(LeaderLine.pointAnchor(marketplaseSberbank, { x: 230, y: 24 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 305, y: 260 }), optionArc)
-let marketOzonLineBig = new LeaderLine(LeaderLine.pointAnchor(marketplaseOzon, { x: 70, y: 0 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 310, y: 100 }), optionArc)
-let marketWbLineBig = new LeaderLine(LeaderLine.pointAnchor(marketplaseWb, { x: 10, y: 24 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 150, y: 85 }), optionArc)
 
+
+
+/*
 
 let wbLine;
 let aliexpressLine;
@@ -51,21 +47,50 @@ let marketSberbankLine;
 let marketOzonLine;
 let marketWbLine;
 
-function refreshArrow() {
+let wbLineBig;
+let aliexpressLineBig;
+let marketAliexpressLineBig;
+let marketYandexLineBig;
+let marketSberbankLineBig;
+let marketOzonLineBig;
+let marketWbLineBig;
+let wbLineMin;
+let aliexpressLineMin;
+let marketAliexpressLineMin;
+let marketYandexLineMin;
+let marketSberbankLineMin;
+let marketOzonLineMin;
+let marketWbLineMin;
+*/
+
+
+let wbLine = new LeaderLine(LeaderLine.pointAnchor(wb, { x: 15, y: 85 }), LeaderLine.pointAnchor(ecomImg, { x: 100, y: 20 }), optionLine)
+let aliexpressLine = new LeaderLine(LeaderLine.pointAnchor(aliexpress, { x: 15, y: 15 }), LeaderLine.pointAnchor(ecomImg, { x: 100, y: 100 }), optionLine)
+let marketAliexpressLine = new LeaderLine(LeaderLine.pointAnchor(marketplaseAliexpress, { x: 75, y: 48 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 30, y: 160 }), optionArc)
+let marketYandexLine = new LeaderLine(LeaderLine.pointAnchor(marketplaseYandex, { x: 110, y: 48 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 110, y: 300 }), optionArc)
+let marketSberbankLine = new LeaderLine(LeaderLine.pointAnchor(marketplaseSberbank, { x: 230, y: 24 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 305, y: 260 }), optionArc)
+let marketOzonLine = new LeaderLine(LeaderLine.pointAnchor(marketplaseOzon, { x: 70, y: 0 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 310, y: 100 }), optionArc)
+let marketWbLine = new LeaderLine(LeaderLine.pointAnchor(marketplaseWb, { x: 10, y: 24 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 150, y: 85 }), optionArc)
+let notebookLine = new LeaderLine(LeaderLine.pointAnchor(imgGroupMarketplace, { x: 0, y: 80 }), LeaderLine.pointAnchor(notebook, { x: 200, y: 20 }), optionArc)
+
+function createLine() {
 
     let screenWidth = window.innerWidth
 
-    // Ширина экрана
+    wbLine.remove()
+    aliexpressLine.remove()
+    marketAliexpressLine.remove()
+    marketYandexLine.remove()
+    marketSberbankLine.remove()
+    marketOzonLine.remove()
+    marketWbLine.remove()
+    notebookLine.remove()
+    console.log("удалить средние")
 
-    if (screenWidth < 1345) {
-        wbLineBig.remove()
-        aliexpressLineBig.remove()
-        marketAliexpressLineBig.remove()
-        marketYandexLineBig.remove()
-        marketSberbankLineBig.remove()
-        marketOzonLineBig.remove()
-        marketWbLineBig.remove()
+    console.log(wbLine)
 
+
+    if (screenWidth > 480 && screenWidth < 1345) {
         wbLine = new LeaderLine(LeaderLine.pointAnchor(wb, { x: 5, y: 55 }), LeaderLine.pointAnchor(ecomImg, { x: 72, y: 15 }), optionLine)
         aliexpressLine = new LeaderLine(LeaderLine.pointAnchor(aliexpress, { x: 7, y: 15 }), LeaderLine.pointAnchor(ecomImg, { x: 72, y: 72 }), optionLine)
         marketAliexpressLine = new LeaderLine(LeaderLine.pointAnchor(marketplaseAliexpress, { x: 40, y: 30 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 15, y: 95 }), optionArc)
@@ -73,35 +98,37 @@ function refreshArrow() {
         marketSberbankLine = new LeaderLine(LeaderLine.pointAnchor(marketplaseSberbank, { x: 140, y: 14 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 180, y: 155 }), optionArc)
         marketOzonLine = new LeaderLine(LeaderLine.pointAnchor(marketplaseOzon, { x: 50, y: 0 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 195, y: 80 }), optionArc)
         marketWbLine = new LeaderLine(LeaderLine.pointAnchor(marketplaseWb, { x: 0, y: 12 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 90, y: 50 }), optionArc)
-    } else {
-        wbLine.remove()
-        aliexpressLine.remove()
-        marketAliexpressLine.remove()
-        marketYandexLine.remove()
-        marketSberbankLine.remove()
-        marketOzonLine.remove()
-        marketWbLine.remove()
+        notebookLine = new LeaderLine(LeaderLine.pointAnchor(imgGroupMarketplace, { x: 0, y: 80 }), LeaderLine.pointAnchor(notebook, { x: 200, y: 20 }), optionArc)
 
-        wbLineBig = new LeaderLine(LeaderLine.pointAnchor(wb, { x: 15, y: 85 }), LeaderLine.pointAnchor(ecomImg, { x: 100, y: 20 }), optionLine)
-        aliexpressLineBig = new LeaderLine(LeaderLine.pointAnchor(aliexpress, { x: 15, y: 15 }), LeaderLine.pointAnchor(ecomImg, { x: 100, y: 100 }), optionLine)
-        marketAliexpressLineBig = new LeaderLine(LeaderLine.pointAnchor(marketplaseAliexpress, { x: 75, y: 48 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 30, y: 160 }), optionArc)
-        marketYandexLineBig = new LeaderLine(LeaderLine.pointAnchor(marketplaseYandex, { x: 110, y: 48 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 110, y: 300 }), optionArc)
-        marketSberbankLineBig = new LeaderLine(LeaderLine.pointAnchor(marketplaseSberbank, { x: 230, y: 24 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 305, y: 260 }), optionArc)
-        marketOzonLineBig = new LeaderLine(LeaderLine.pointAnchor(marketplaseOzon, { x: 70, y: 0 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 310, y: 100 }), optionArc)
-        marketWbLineBig = new LeaderLine(LeaderLine.pointAnchor(marketplaseWb, { x: 10, y: 24 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 150, y: 85 }), optionArc)
+
+    } else if (screenWidth > 1345) {
+        wbLine = new LeaderLine(LeaderLine.pointAnchor(wb, { x: 15, y: 85 }), LeaderLine.pointAnchor(ecomImg, { x: 100, y: 20 }), optionLine)
+        aliexpressLine = new LeaderLine(LeaderLine.pointAnchor(aliexpress, { x: 15, y: 15 }), LeaderLine.pointAnchor(ecomImg, { x: 100, y: 100 }), optionLine)
+        marketAliexpressLine = new LeaderLine(LeaderLine.pointAnchor(marketplaseAliexpress, { x: 75, y: 48 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 30, y: 160 }), optionArc)
+        marketYandexLine = new LeaderLine(LeaderLine.pointAnchor(marketplaseYandex, { x: 110, y: 48 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 110, y: 300 }), optionArc)
+        marketSberbankLine = new LeaderLine(LeaderLine.pointAnchor(marketplaseSberbank, { x: 230, y: 24 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 305, y: 260 }), optionArc)
+        marketOzonLine = new LeaderLine(LeaderLine.pointAnchor(marketplaseOzon, { x: 70, y: 0 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 310, y: 100 }), optionArc)
+        marketWbLine = new LeaderLine(LeaderLine.pointAnchor(marketplaseWb, { x: 10, y: 24 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 150, y: 85 }), optionArc)
+        notebookLine = new LeaderLine(LeaderLine.pointAnchor(imgGroupMarketplace, { x: 0, y: 80 }), LeaderLine.pointAnchor(notebook, { x: 200, y: 20 }), optionArc)
+
+
+    } else {
+        wbLine = new LeaderLine(LeaderLine.pointAnchor(wb, { x: 5, y: 35 }), LeaderLine.pointAnchor(ecomImg, { x: 50, y: 10 }), optionLine)
+        aliexpressLine = new LeaderLine(LeaderLine.pointAnchor(aliexpress, { x: 5, y: 10 }), LeaderLine.pointAnchor(ecomImg, { x: 50, y: 50 }), optionLine)
+        marketAliexpressLine = new LeaderLine(LeaderLine.pointAnchor(marketplaseAliexpress, { x: 32, y: 24 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 15, y: 70 }), optionArc)
+        marketYandexLine = new LeaderLine(LeaderLine.pointAnchor(marketplaseYandex, { x: 60, y: 24 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 50, y: 120 }), optionArc)
+        marketSberbankLine = new LeaderLine(LeaderLine.pointAnchor(marketplaseSberbank, { x: 100, y: 12 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 120, y: 105 }), optionArc)
+        marketOzonLine = new LeaderLine(LeaderLine.pointAnchor(marketplaseOzon, { x: 35, y: 0 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 120, y: 40 }), optionArc)
+        marketWbLine = new LeaderLine(LeaderLine.pointAnchor(marketplaseWb, { x: 0, y: 12 }), LeaderLine.pointAnchor(arrowRoundImg, { x: 75, y: 40 }), optionArc)
+        notebookLine = new LeaderLine(LeaderLine.pointAnchor(imgGroupMarketplace, { x: 0, y: 50 }), LeaderLine.pointAnchor(notebook, { x: 120, y: 10 }), optionArc)
     }
 }
-refreshArrow
-window.addEventListener('load', refreshArrow)
-window.addEventListener('resize', refreshArrow)
+createLine()
 
-
+window.addEventListener('load', createLine)
+window.addEventListener('resize', createLine)
 
 new LeaderLine(b, a, optionLine);
 new LeaderLine(yandex, ecomImg, optionLine);
 new LeaderLine(sberbank, ecomImg, optionLine);
 new LeaderLine(ozon, ecomImg, optionLine);
-new LeaderLine(LeaderLine.pointAnchor(imgGroupMarketplace, { x: 0, y: 80 }), LeaderLine.pointAnchor(notebook, { x: 325, y: 0 }), optionArc)
-
-
-
